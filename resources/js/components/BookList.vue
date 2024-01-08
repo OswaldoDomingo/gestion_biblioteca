a59c0e748fbacb54142f84e44eb2c73ba282897e
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2024-01-08 08:55:53 +0100

    Video Sesiones y cookies Instalación js-cookie Añadir botón add favirite a los libros Añadir métodos a BookList.vue

--- resources/js/components/BookList.vue
+++ resources/js/components/BookList.vue
@@ -4,6 +4,12 @@
         <ul>
             <li v-for="book in books" :key="book.id">
                 {{ book.title }} - {{ book.published_year }}
+                <!-- Se le añade un botón -->
+                <button @click="addFavorito(book.ide)"
+                    class="px-3 py-1 ml-2 bg-blue-500 hover:bg-blue-700 text-white rounded">
+                    Add Fav
+                </button>
+
             </li>
         </ul>
     </div>
@@ -18,6 +24,9 @@
 </template>
 
 <script>
+//Se importa la librería de cookies
+import Cookies from 'js-cookie';
+
 export default {
     //con que datos vamos a trabajar
     data() {
@@ -42,6 +51,43 @@ export default {
             .then((data) => {
                 this.authors = data;
             });
+    },
+    methods: {
+        addFavorito(id) {
+            let favorites = Cookies.get('favorites');
+            if (!favorites) {
+                favorites = '[]';
+            }
+            //Parseo lista
+            favorites = JSON.parse(favorites);
+
+            //agrergar nuevo a favorito
+            if (!favorites.includes(id)) {
+                favorites.push(id);
+                Cookies.set('favorites', JSON.stringify(favorites), { expires: 30 });
+            }
+            //Método, addFavorito(id), se utiliza para agregar un libro a la lista de favoritos del usuario.
+
+            // let favorites = Cookies.get('favorites');: Aquí, estás obteniendo la lista de favoritos del usuario de las cookies. Si el usuario no tiene ninguna lista de favoritos, favorites será undefined.
+
+            // if(!favorites){ favorites = '[]'; }: Si favorites es undefined (lo que significa que el usuario no tiene ninguna lista de favoritos), inicializas favorites como una cadena que representa un array vacío.
+
+            // favorites = JSON.parse(favorites);: Aquí, estás convirtiendo la cadena favorites en un array de JavaScript. Esto es necesario porque las cookies solo pueden almacenar datos como cadenas.
+
+            // if(!favorites.includes(id)){ ... }: Este bloque de código se ejecuta si el id del libro no está ya en la lista de favoritos. Utilizas el método includes para comprobar si el id ya está en el array favorites.
+
+            // favorites.push(id);: Si el id no está en la lista de favoritos, lo agregas al array favorites con el método push.
+
+            // Cookies.set('favorites', JSON.stringify(favorites) , {expires : 30});: Finalmente, se guarda la lista de favoritos actualizada en las cookies. Primero, se convierte el array favorites en una cadena con JSON.stringify, ya que las cookies solo pueden almacenar cadenas. Luego, se establece la cookie 'favorites' con esta cadena. La cookie expirará después de 30 días.
+
+            // Este método permite a los usuarios agregar un libro a su lista de favoritos. La lista de favoritos se guarda en las cookies para que persista incluso después de que el usuario cierre el navegador.
+           
+            //Actualizar la variable
+            this.favorites = favorites;
+
+            console.log(favorites);
+        }
+
     }
 }
 </script>
